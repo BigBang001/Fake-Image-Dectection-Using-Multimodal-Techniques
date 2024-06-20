376f12c1d7fac7859fff7a6f7c3e6f6c4b7a1405
Ekamjot Kaur <119509166+BigBang001@users.noreply.github.com>
2024-06-20 16:40:26 +0530

    Create metadata_model.py
    This file contains the code for training the metadata model.

---
+++ metadata_model.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+
+def train_metadata_model(X_metadata, y_metadata):
+    metadata_model = RandomForestClassifier()
+    metadata_model.fit(X_metadata, y_metadata)
+    return metadata_model
